43a6b16863e9c8a0fbcc034113b204e98de60dbf
Payton Boone <paytonboone30@gmail.com>
2023-02-01 15:00:26 -0700

    House class is created

--- app/Models/House.js
+++ app/Models/House.js
@@ -1,3 +1,16 @@
 
 
-export class House { }
+export class House { 
+    constructor(data) {
+        this.id = generateId()
+        this.year = data.year
+        this.name = data.name
+        this.bedrooms = data.bedrooms
+        this.bathrooms = data.bathrooms
+        this.sqft = data.sqft
+        this.price = data.price
+        this.description = data.description
+        this.imgUrl = data.imgUrl
+    }
+    
+}
