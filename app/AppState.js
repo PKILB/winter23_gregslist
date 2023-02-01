c89f933fba400199c021f10ac860de1193bc4264
Payton Boone <paytonboone30@gmail.com>
2023-02-01 15:07:05 -0700

    Houses array and Active House added to appstate

--- app/AppState.js
+++ app/AppState.js
@@ -1,4 +1,5 @@
 import { Car } from "./Models/Car.js"
+import { House } from "./Models/House"
 import { Value } from "./Models/Value.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
@@ -12,6 +13,22 @@ class AppState extends EventEmitter {
   cars = loadState('cars', [Car])
   /** @type {import('./Models/Car').Car} */
   car = null
+
+  /** @type {import('./Models/House').House} */
+  houses = [
+    new House(
+      '1974',
+      'Mid-Century Modern Single Family Home',
+      3,
+      2.5,
+      1400,
+      375000, 
+      'Newly renovated mid-century modern home', 
+      'https://rew-feed-images.global.ssl.fastly.net/imls/_cloud_media/property/residentialincome/98862307-1-835976ecc96808a93ea194115ae6c537-m.jpg')
+  ]
+
+  /** @type {import('./Models/House').House} */
+  house = null
 }
 
 export const appState = new Proxy(new AppState(), {
